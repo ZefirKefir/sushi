6d13bf2c4ebf09c209a5d613a9aac6db54bb6c49
Zefir Kefir <mr.yunusov.00@gmail.com>
2024-02-02 00:38:54 +0100

    missing key

--- app/(components)/Main.tsx
+++ app/(components)/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
     <main className="grow bg-purple-200 overflow-y-scroll hide-scrollbar">
       <div className="flex flex-col gap-1.5 bg-[#eee] p-3">
         {data.rolls.map(item => (
-          <ItemCard title={item.name} type={item.section} inside={item.inside} outside={item.outside} feature={item?.feature} />
+          <ItemCard key={item.name} title={item.name} type={item.section} inside={item.inside} outside={item.outside} feature={item?.feature} />
         ))}
       </div>
     </main>
